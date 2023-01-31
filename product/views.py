fc7032d11c75586cb619b9b951d8cc9661e57ff6
jorgeav527 <jorgeav527@gmail.com>
2023-01-31 09:33:03 -0500

    setup vue and django & missing .gitignore and README.md and licence

--- product/views.py
+++ product/views.py
@@ -16,17 +16,20 @@ class LatestProductsList(APIView):
         return Response(serializer.data)
 
 
-# class ProductDetail(APIView):
-#     def get_object(self, category_slug, product_slug):
-#         try:
-#             return Product.objects.filter(category__slug=category_slug).get(slug=product_slug)
-#         except Product.DoesNotExist:
-#             raise Http404
+class ProductDetail(APIView):
+    def get_object(self, category_slug, product_slug):
+        try:
+            return Product.objects.filter(category__slug=category_slug).get(
+                slug=product_slug
+            )
+        except Product.DoesNotExist:
+            raise Http404
 
-#     def get(self, request, category_slug, product_slug, format=None):
-#         product = self.get_object(category_slug, product_slug)
-#         serializer = ProductSerializer(product)
-#         return Response(serializer.data)
+    def get(self, request, category_slug, product_slug, format=None):
+        product = self.get_object(category_slug, product_slug)
+        serializer = ProductSerializer(product)
+        return Response(serializer.data)
+
 
 # class CategoryDetail(APIView):
 #     def get_object(self, category_slug):
